2a58ae94d66a82d431bf43fe0327458cbf7e4457
Rahul Reddy S <rahul.sannapureddy@gmail.com>
2025-12-29 10:14:23 +0000

    updated inverted_index.h to include more functions.

--- src/engine_cpp/inverted_index.h
+++ src/engine_cpp/inverted_index.h
@@ -8,10 +8,14 @@ class InvertedIndex
 public:
     void add_document(int doc_id, const std::string &text);
     const std::vector<std::pair<int, int>> &get_postings(const std::string &term) const;
+    int get_document_count() const;
+    int get_document_length(int doc_id) const;
 
 private:
     std::unordered_map<std::string, std::unordered_map<int, int>> tempIndex;
     std::unordered_map<std::string, std::vector<std::pair<int, int>>> Index;
     void finalizeIndex();
     void add_token(const std::string &token, int doc_id);
+    std::unordered_map<int, int> doc_lengths;
+    int total_docs = 0;
 };

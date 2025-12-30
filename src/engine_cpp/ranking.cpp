57351be2f1fc120e86a829839cfc4a820dea6115
Rahul Reddy S <rahul.sannapureddy@gmail.com>
2025-12-30 04:23:11 +0000

    Modified ranking.cpp to remove namespace std.

--- src/engine_cpp/ranking.cpp
+++ src/engine_cpp/ranking.cpp
@@ -2,20 +2,17 @@
 #include <unordered_map>
 #include <sstream>
 #include <algorithm>
-#include <cmath>
-using namespace std;
 
 Ranker::Ranker(double k1_, double b_) : k1(k1_), b(b_) {}
 
 std::vector<int> Ranker::rank(const InvertedIndex &index, const std::string &query, int k) const
 {
-    unordered_map<int, double> scores;
+    std::unordered_map<int, double> scores;
 
-    istringstream iss(query);
-    string term;
+    std::istringstream iss(query);
+    std::string term;
 
     double avg_doc_length = index.get_avg_document_length();
-    int N = index.get_document_count();
 
     while (iss >> term)
     {
@@ -30,16 +27,16 @@ std::vector<int> Ranker::rank(const InvertedIndex &index, const std::string &que
             int doc_id = posting.first;
             int tf = posting.second;
             int doc_length = index.get_document_length(doc_id);
-            double score = idf * (tf * (k1 + 1)) / (tf + k1) * (doc_length / avg_doc_length);
+            double score = idf * (tf * (k1 + 1)) / (tf + k1 * (1 - b + b * (doc_length / avg_doc_length)));
             scores[doc_id] += score;
         }
     }
 
-    vector<pair<int, double>> scored_docs(scores.begin(), scores.end());
-    sort(scored_docs.begin(), scored_docs.end(), [](const auto &a, const auto &b)
-         { return a.second > b.second; });
+    std::vector<std::pair<int, double>> scored_docs(scores.begin(), scores.end());
+    std::sort(scored_docs.begin(), scored_docs.end(), [](const auto &a, const auto &b)
+              { return a.second > b.second; });
 
-    vector<int> results;
+    std::vector<int> results;
     for (int i = 0; i < k && i < scored_docs.size(); i++)
     {
         results.push_back(scored_docs[i].first);
